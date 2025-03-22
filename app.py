d4079fe9ffb609bfa0b1123ed7cbc2a44dc215a8
k3vster <masterlord37@gmail.com>
2025-03-22 12:44:21 -0700

    updates and test

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("index.html", myvalue="heyo")
+
+
+@app.route("/thug", methods=["GET", "POST", "THUG"])
+def hello():
+    if request.method == "GET":
+        return "GET REQUEST", 200
+    elif request.method == "POST":
+        return "POST REQUEST"
+    else:
+        return "YOOOOO!"
+
+
+@app.route("/test", methods=["GET", "POST", "THUG"])
+def test():
+    response = make_response()
+    response.status_code = 200
+
+    # response.headers["content-type"] = "application/octet-stream"
+    return f"{vars(response)}"
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    return f"whattup {name}"
+
+@app.route("/pow/<int:num1>/<int:num2>")
+def sitepow(num1: int, num2: int) -> str:
+    return f"{pow(num1,num2)}"
+
+
+@app.route("/handle_url_params")
+def handle_params():
+    name = request.args.get("name")
+    pw = request.args.get("pw")
+    greet = request.args.get("greet")
+
+    return f"{name} {pw} {greet}"
+
 
 
 
